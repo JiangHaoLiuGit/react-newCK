af67cbb01e80b6a8a2b75a3c85e14db25cae946b
liujianghao <liujianghao@aspirecn.com>
2024-03-27 16:30:51 +0800

    简化内容

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+let div = (
   <div>
-    <h1>
-      React
-    </h1>
+    <h1>React</h1>
   </div>
-);
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root')).render(div);
+
 
