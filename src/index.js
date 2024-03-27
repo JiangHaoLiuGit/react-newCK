993b5b943f31ba31d3a970a1c3e71f108b75c551
liujianghao <liujianghao@aspirecn.com>
2024-03-27 18:03:19 +0800

    图片

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import src1 from './assets/1.jpg'
+import src2 from './assets/2.jpg'
+import src3 from './assets/3.jpg'
+import './index.css'
 
+let imgArr = [src1,src2,src3]
+let index = 0
+let time = null
+let imgDiv = document.getElementById("root")
 
-let div = (
-  <div>
-    <h1>React</h1>
-  </div>
-)
+function start(){
+  if(!time){
+    time = setInterval(()=>{
+      console.log(index)
+      index = (index + 1) % 3 
+      render()
+    },1000)
+  }
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root')).render(div);
+function stop(){
+  clearInterval(time)
+  time = null
+}
 
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
+function render(){
+  root.render(
+    <React.StrictMode>
+      <div id='img'>
+        <img src={imgArr[index]} alt=''/>
+      </div>
+    </React.StrictMode>
+  );
+}
+
+render()
+
+start()
+
+imgDiv.onmouseenter = () => {
+  start()
+}
+
+imgDiv.onmouseleave = () => {
+  stop()
+}
