272ac5b5131f66904b1d82651e7124a68a7e45d9
liujianghao <liujianghao@aspirecn.com>
2024-05-13 17:24:05 +0800

    hook简介

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Test from './view/Profiler'
+import Test from './view/HOOK'
 
 export default class App extends React.Component{
     render(){
---
+++ src/view/HOOK/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Test extends Component {
+  render() {
+    return (
+      <div>
+        <h1>HOOK</h1>
+      </div>
+    )
+  }
+}
