15a2cbe68f357f1066f62ee7d1831c9a1470ff0d
liujianghao <liujianghao@aspirecn.com>
2024-07-31 17:40:08 +0800

    暂存

--- src/view/ReactRouterAnimation/index.js
+++ src/view/ReactRouterAnimation/index.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React , {useState} from 'react'
 import * as Page from './page.js'
 import {BrowserRouter as Router , Route} from 'react-router-dom'
+import {CSSTransition} from 'react-transition-group'
 import './index.css'
+import 'animate.css'
+
+function MyTransition({visible, children} ){
+    return <CSSTransition appear mountOnEnter in={visible} timeout={500} classNames={{
+        exitActive:"animate__fadeOutLeft",
+        exitDone:"exit-done",
+        enterActive:"animate__fadeInRight",
+        appearActive:"animate__fadeInRight"
+    }}>
+        {children}
+    </CSSTransition>
+}
+
 export default function Test() {
+    let [hide , setHide ] = useState(true)
   return (
     <Router>
         <div className='container'>
@@ -10,9 +25,55 @@ export default function Test() {
 
             </Page.navPage>
             <div className='box'>
-                <Route path="/home" component={Page.Home}></Route>
-                <Route path="/news" component={Page.News}></Route>
-                <Route path="/personal" component={Page.Personal}></Route>
+                <Route path="/" exact>
+                    {({match})=>{
+                        //  component={Page.Home}
+                        console.log(match)
+                        if(match){
+                            return <MyTransition visible={hide}>
+                                <Page.Home/>
+                            </MyTransition>
+                            
+                        }else{
+                            return <MyTransition visible={!hide}>
+                                <Page.Home/>
+                            </MyTransition>
+                        }
+                    }}
+                </Route>
+                <Route path="/news" exact >
+                    {({match})=>{
+                        //  component={Page.Home}
+                        console.log(match)
+                        if(match){
+                            return <MyTransition visible={hide}>
+                                <Page.News/>
+                            </MyTransition>
+                            
+                        }else{
+                            return <MyTransition visible={!hide}>
+                                <Page.News/>
+                            </MyTransition>
+                        }
+                    }}
+                </Route>
+
+                <Route path="/personal" exact >
+                    {({match})=>{
+                        //  component={Page.Home}
+                        console.log(match)
+                        if(match){
+                            return <MyTransition visible={hide}>
+                                <Page.Personal/>
+                            </MyTransition>
+                            
+                        }else{
+                            return <MyTransition visible={!hide}>
+                                <Page.Personal/>
+                            </MyTransition>
+                        }
+                    }}
+                </Route>
             </div>
         </div>
     </Router>
--- src/view/ReactRouterAnimation/page.js
+++ src/view/ReactRouterAnimation/page.js
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom'
 export function navPage() {
   return (
     <div className='nav'>
-      <NavLink to="/home">首页</NavLink>
+      <NavLink to="/">首页</NavLink>
       <NavLink to="/news">新闻页</NavLink>
       <NavLink to="/personal">个人中心</NavLink>
     </div>
@@ -12,13 +12,13 @@ export function navPage() {
 }
 
 export function Home(){
-    return <div className='home'><h1>首页</h1></div>
+    return <div className='home animate__animated animate__faster'><h1>首页</h1></div>
 }
 
 export function News(){
-    return <div className='news'><h1>新闻页</h1></div>
+    return <div className='news animate__animated animate__faster'><h1>新闻页</h1></div>
 }
 
 export function Personal(){
-    return <div className='personal'><h1>个人中心</h1></div>
+    return <div className='personal animate__animated animate__faster'><h1>个人中心</h1></div>
 }
