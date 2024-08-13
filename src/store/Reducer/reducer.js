44fd6d640e6911260d4cdeb5ce326e18536b2e26
liujianghao <liujianghao@aspirecn.com>
2024-08-13 10:41:50 +0800

    更新

--- src/store/Reducer/reducer.js
+++ src/store/Reducer/reducer.js
@@ -4,23 +4,11 @@ const defaultState = {
     str:'前段开发'
 }
 
-const reducerOne = (state = defaultState,action)=>{
-    // let newState = JSON.parse(JSON.stringify(state))
-    // switch(action.type){
-    //     case 'change_active':
-    //         newState.str = action.value
-    //     default:
-    //         break;
-    // }
-    // return newState
+export const reducer = (state = defaultState,action)=>{
     switch(action.type){
         case 'change_active':
             return Object.assign({},state,action)
         default:
-            console.log("?")
             return state;
     }
-}
-module.exports = {
-    reducerOne
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 // 主要是用来创建仓库的,引用reducer,并导出状态
-import {reducerOne} from './Reducer/reducer'
-import { createStore } from 'redux' ;
+import {reducer} from './reducer/reducer.js'
+import { createStore } from 'redux'
 
-export const store = createStore(reducerOne)
+export const store = createStore(reducer)
