bc21f52547a73bf30e5cf138ce65c0a14febade0
LiuJiangHao <jianghaoliu@yeah.net>
2025-04-30 12:06:36 +0800

    增加弹窗校验

--- src/component/Model/Model.js
+++ src/component/Model/Model.js
@@ -2,9 +2,11 @@ import React from 'react'
 import './Model.css'
 
 export default function Model(props){
+    console.log(props);
+    let item = props.show ? (<div className='modelDiv'>
+        <p>加载中...</p>
+    </div>):''
     return <>
-        <div className='modelDiv'>
-            <p>加载中...</p>
-        </div>
+        {item}
     </>
 }
