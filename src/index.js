99b9535f16e33f657a8b8c86a8b8e6b1fa888afb
LiuJiangHao <jianghaoliu@yeah.net>
2025-04-30 11:55:57 +0800

    接口失效,注释

--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ async function fetchAllStudents(){
                .then(res=>res.json()).then(res=>res).then(res=>res)
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
-fetchAllStudents()
+// fetchAllStudents()
 async function render(){
   root.render(
     <>
       <h1>正在加载中...</h1>
     </>
   )
-  let studentList = await fetchAllStudents()
+//   await fetchAllStudents()
+  let studentList = []
   root.render(
     <>
       <StudentList strList={studentList}/>
