8b875de50cd2611a3eb36a150c313072b0e76a76
LiuJiangHao <jianghaoliu@yeah.net>
2025-05-03 19:39:49 +0800

    修改代码

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Tem from './tem.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 function render(){
-  root.render(<Tem children2="我是父元素2内容"  children3="我是父元素3内容">
+  root.render(<Tem children2={<h2>我是父元素2内容</h2>}  children3="我是父元素3内容">
     <h1>我是父元素1内容</h1>
   </Tem>)
 }
