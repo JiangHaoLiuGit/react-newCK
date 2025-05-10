ccada29dbcd2fe58d6e6047232648094493aafe5
LiuJiangHao <jianghaoliu@yeah.net>
2025-05-10 20:59:25 +0800

    补充知识点

--- src/view/ReactDomEvent/index.js
+++ src/view/ReactDomEvent/index.js
@@ -2,21 +2,24 @@ import React from 'react'
 let parent
 export default function Test(){
     return <div onClick={(e) => {
+        e.type = "keyUp"
         console.log("我是虚拟dom中的父级div")
-        console.log(e,parent,parent === e)
+        console.log("e:",e,"parent:",parent,"parent === e:",parent === e)
     }}> 
-    <input type="text" onFocus={e=>{
-        console.log("e啊")
-    }} />
+        <input type="text" onFocus={e=>{
+            console.log("input e啊")
+        }} />
         <button onClick={(e) => {
             // 这里阻止事件冒泡阻止不了真实DOM的事件冒泡
             // e.stopPropagation()
+            
             console.log("虚拟dom元素:button点击",e)
             parent = e
             setTimeout(()=>{
+                //这里很神奇打印第五行设置的 "keyUp" 说明什么?说明react元素生成的dom事件中的e全局公用一个event对象
                 console.log(e.type)
             },1000)
-            // console.log(e.nativeEvent)
+            console.log(e.nativeEvent)
         }}>点击</button>
     </div>
 }
@@ -24,7 +27,7 @@ export default function Test(){
 document.querySelector("#root").onclick = e => {
     console.log("真实dom元素:root元素点击",e)
     // 真实dom中阻止冒泡,注意,这里会阻止后续react中所有虚拟dom的事件运行
-    // e.stopPropagation()
+    e.stopPropagation()
 }
 document.querySelector("#app").onclick = e => {
     console.log("真实dom元素:app元素点击",e)
