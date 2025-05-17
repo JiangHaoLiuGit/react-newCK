d31766ce0c4435fb6274b874edf89fefcf177a30
LiuJiangHao <jianghaoliu@yeah.net>
2025-05-17 21:11:09 +0800

    补充

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 // 主要是用来创建仓库的,引用reducer,并导出状态
-import {reducer} from './reducer/reducer.js'
+import {reducer} from './reducer'
 import { createStore } from 'redux'
 
 export const store = createStore(reducer)
