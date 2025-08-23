d8200fedd2afb8f63a0bdf4786020efb2f28b721
LiuJiangHao <jianghaoliu@yeah.net>
2025-08-24 01:34:01 +0800

    补充

--- src/view/ReactRouterProtect/ProtectRouter.js
+++ src/view/ReactRouterProtect/ProtectRouter.js
@@ -2,7 +2,8 @@ import React from 'react'
 import {Route,Redirect} from 'react-router-dom'
 import data from './isLogin'
 import Login from './Login'
-// children , component:Component , render 为什么结构这三个属性,这三个属性不能给这个组件,所以要抽离出来
+// 通用性代码,对route组件的二次封装成高阶组件!!!
+// children , component:Component , render 为什么解构这三个属性,这三个属性不能给这个组件,所以要抽离出来
 // 剩下的属性path 默认的直接都给
 export default function ProtectRouter(props) {
   console.log(props);
