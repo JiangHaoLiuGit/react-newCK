98167ae0fc0ddfda253b3360ea9366bb260f8bf4
liujianghao <jianghaoliu@yeah.net>
2026-01-13 19:32:09 +0800

    补充

--- src/component/studentList.js
+++ src/component/studentList.js
@@ -3,7 +3,10 @@ import Student from './student.js'
 export default class StudentList extends React.Component{
     render(){
         return <ul>
-            {this.props.strList.map(res => <Student key={res.title} {...res}/>)}
+            {this.props.strList && this.props.strList.length > 0?
+            this.props.strList.map(res => <Student key={res.title} {...res}/>)
+            :
+            <Student />}
         </ul>
     }
 }
